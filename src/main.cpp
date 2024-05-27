b1aef052def6e1f79d7e4f4778bf17d8510312c0
Ezequiel Ubiedo <lautaroezequielubiedo@gmail.com>
2024-05-27 20:20:28 -0300

    Control de Nivel con Potenciómetro

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,84 @@
 #include <Arduino.h>
 
+// CONSTANTES SENSORES
+#define KInput 0.201
+#define KOutput 0.218
+
+// PINES
+const byte FlowmeterIn = 14,  // Sensor de entrada al tanque
+           FlowmeterOut = 12, // Sensor de salida del tanque
+           StandBy = 5,       // Enable TB6612FNG
+           WaterPump = 4,     // Bomba de Agua
+           Adjust = A0;       // ADC
+
+// VARIABLES
+int PWMset = 0;
+double QIn = 0, QOut = 0; // Caudales medidos
+volatile int CountIn = 0, CountOut = 0; //Contadores de pulsos
+unsigned long TimeRef = 0; 
+
 // put function declarations here:
-int myFunction(int, int);
+void FlowIn();
+void FlowOut();
+void SetPins();
+void SendData(double x1, double x2);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  SetPins();
+  digitalWrite(StandBy,LOW);
+  analogWriteRange(1023);
+  analogWriteFreq(1000);
+  attachInterrupt(digitalPinToInterrupt(FlowmeterIn),FlowIn,RISING);
+  attachInterrupt(digitalPinToInterrupt(FlowmeterOut),FlowOut,RISING);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  TimeRef = millis();
+  interrupts();
+  CountIn = 0;
+  CountOut = 0;
+  while((millis() - TimeRef) < 1000){
+    QIn = (CountIn * KInput);
+    QOut = (CountOut * KOutput);
+    PWMset = analogRead(Adjust);
+    analogWrite(WaterPump, PWMset);
+  }
+  noInterrupts();
+  if(abs(QIn-QOut) < (0.05*QOut)){
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else{
+    digitalWrite(LED_BUILTIN,LOW);
+  }
+  SendData(QIn,QOut);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void FlowIn() {
+  CountIn++;
+}
+
+void FlowOut() { 
+  CountOut++;
+}
+
+void SendData(double x1, double x2) {
+  Serial.print("QIn=");
+  Serial.print(x1);
+  Serial.print("ml/s");
+  Serial.print("  QOut=");
+  Serial.print(x2);
+  Serial1.println("ml/s");
+}
+
+void SetPins() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(FlowmeterIn, INPUT);
+  pinMode(FlowmeterOut, INPUT);
+  pinMode(StandBy, OUTPUT);
+  pinMode(WaterPump, OUTPUT);
+  pinMode(Adjust,INPUT);
 }
