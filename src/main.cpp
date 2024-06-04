a400161513e92416a07675279c03266c0eb5f937
Ezequiel Ubiedo <lautaroezequielubiedo@gmail.com>
2024-06-04 12:45:58 -0300

    Arreglo de Errores de asignación en IRAM de las funciones de interrupción Añadido de calibración del fondo del tanque con el ultrasonido Modificación del envío de datos Modificación de la constante del caudalímetro de entrada

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,19 @@
 #include <Adafruit_BMP085.h>
 
 // CONSTANTES
-#define KInput 0.201
+#define KInput 0.210
 #define KOutput 0.218
 #define SOUND_VELOCITY 331 // m/s
 
 // PINES
-const byte FlowmeterIn = 14,  // Sensor de entrada al tanque
-           FlowmeterOut = 12, // Sensor de salida del tanque
-           StandBy = 13,      // Enable TB6612FNG
-           WaterPump = 15,    // Bomba de Agua
+const byte FlowmeterIn = 14,  // (D5) Sensor de entrada al tanque
+           FlowmeterOut = 12, // (D6) Sensor de salida del tanque
+           StandBy = 13,      // (D7) Enable TB6612FNG
+           WaterPump = 15,    // (D8) Bomba de Agua
            Adjust = A0,       // ADC
-           Trigger = 0,       // Emisor del HC-SR04
-           Echo = 2;          // Receptor del HC-SR04
+           Trigger = 0,       // (D3) Emisor del HC-SR04
+           Echo = 2;          // (D4) Receptor del HC-SR04
+Adafruit_BMP085 bmp;          // D1=SCL D2=SDA Sensor de Temperatura
 
 // VARIABLES
 int PWMset = 0;
@@ -23,22 +24,20 @@ double QIn = 0, QOut = 0; // Caudales medidos
 volatile int CountIn = 0, CountOut = 0; //Contadores de pulsos
 unsigned long TimeRef = 0; 
 long Duration;
-float Distance;
-Adafruit_BMP085 bmp;
-float SoundVel;
+float Distance,Height,Level,SoundVel;
 
 // put function declarations here:
 void SetPins();
-void FlowIn();
-void FlowOut();
+void IRAM_ATTR FlowIn();
+void IRAM_ATTR FlowOut();
 long UltrasonicSensor(byte TPin, byte EPin);
 float SetSoundVelocity();
-void SendData(double x1, double x2, float x3);
+float SetZero(float SoundVelocity);
+void SendData(double x1, double x2, float x3, int x4);
 
 void setup() {
   // put your setup code here, to run once:
   SetPins();
-  digitalWrite(StandBy,LOW);
   analogWriteRange(1023);
   analogWriteFreq(1000);
   attachInterrupt(digitalPinToInterrupt(FlowmeterIn),FlowIn,RISING);
@@ -49,14 +48,21 @@ void setup() {
     while(1){}
   }
   SoundVel = SetSoundVelocity();
+  Height = SetZero(SoundVel);
+  Serial.print("Distancia al Fondo:");
+  Serial.print(Height);
+  Serial.println("[cm].");
+  delay(5000);
+  digitalWrite(StandBy, HIGH);
+  analogWrite(WaterPump,PWMset);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  TimeRef = millis();
-  interrupts();
   CountIn = 0;
   CountOut = 0;
+  TimeRef = millis();
+  interrupts();
   while((millis() - TimeRef) < 1000){
     QIn = (CountIn * KInput);
     QOut = (CountOut * KOutput);
@@ -66,15 +72,16 @@ void loop() {
   noInterrupts();
   Duration = UltrasonicSensor(Trigger, Echo);
   Distance = Duration * SoundVel/2;
-  SendData(QIn,QOut,Distance);
+  Level = Height - Distance;
+  SendData(QIn,QOut,Level,PWMset);
 }
 
 // put function definitions here:
-void FlowIn() {
+void IRAM_ATTR FlowIn() {
   CountIn++;
 }
 
-void FlowOut() { 
+void IRAM_ATTR FlowOut() { 
   CountOut++;
 }
 
@@ -99,16 +106,30 @@ float SetSoundVelocity(){
   return SV;
 }
 
-void SendData(double x1, double x2, float x3) {
+float SetZero(float SoundVelocity){
+  long TimeMeasured = 0;
+  float DistanceMeasured = 0.0,Zero = 0.0;
+  delay(1000);
+  for (int i = 0; i < 10; i++)
+  {
+    TimeMeasured = UltrasonicSensor(Trigger,Echo);
+    DistanceMeasured = TimeMeasured * SoundVelocity/2;
+    Zero += DistanceMeasured;
+    delay(1000);
+  }
+  Zero = Zero / 10.00;
+  return Zero;
+}
+
+void SendData(double x1, double x2, float x3, int x4) {
   Serial.print("QIn=");
   Serial.print(x1);
-  Serial.print("ml/s");
-  Serial.print("  QOut=");
+  Serial.print("[ml/s]  Qout=");
   Serial.print(x2);
-  Serial.print("ml/s");
-  Serial.print("  H=");
+  Serial.print("[ml/s]  H=");
   Serial.print(x3);
-  Serial.println("cm");
+  Serial.print("[cm]  PWM=");
+  Serial.println(x4);
 }
 
 void SetPins() {
